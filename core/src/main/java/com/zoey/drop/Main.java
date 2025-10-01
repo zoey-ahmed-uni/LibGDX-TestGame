94124ddae5f33086005c6174c0c1ea157f4da4ca
Zoey Ahmed <zoethetransrat@gmail.com>
2025-10-01 13:14:46 +0100

    meta: Inital commit

---
+++ core/src/main/java/com/zoey/drop/Main.java
@@ -0,0 +1,40 @@
+package com.zoey.drop;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    @Override
+    public void create() {
+        // Prepare your application here.
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
+        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+        if(width <= 0 || height <= 0) return;
+
+        // Resize your application here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void render() {
+        // Draw your application here.
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy application's resources here.
+    }
+}
